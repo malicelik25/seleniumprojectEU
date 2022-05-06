9ec4cd6cd4080da3205071fc210e7ec8ededbe1c
Muhammed Ali Çelik <muhammedalicelikk@gmail.com>
2022-05-06 21:18:42 +0300

    T2_getText_getAttribute

--- src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T2_getText_getAttribute.java
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T2_getText_getAttribute.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
-public class T3_getText_getAttribute {
+public class T2_getText_getAttribute {
 
     public static void main(String[] args) {
 
