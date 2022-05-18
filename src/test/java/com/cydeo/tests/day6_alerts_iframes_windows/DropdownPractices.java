e9defb47fb9e899d50d92f2380c876e807b83e7f
Muhammed Ali Çelik <muhammedalicelikk@gmail.com>
2022-05-18 16:23:29 +0300

    Dropdown Practices

--- src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/DropdownPractices.java
+++ src/test/java/com/cydeo/tests/day6_alerts_iframes_windows/DropdownPractices.java
@@ -5,21 +5,27 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
 public class DropdownPractices {
 
-    @Test
-    public void dropdown_task5() throws InterruptedException {
-        //TC #5: Selecting state from State dropdown and verifying result
-        //1. Open Chrome browser
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setupMethod(){
         //2. Go to http://practice.cydeo.com/dropdown
-        WebDriver driver = WebDriverFactory.getDriver("chrome");
+        driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("http://practice.cydeo.com/dropdown");
+    }
+
+    @Test
+    public void dropdown_task5() throws InterruptedException {
+        //TC #5: Selecting state from State dropdown and verifying result
 
         //we located the dropdown and it is ready to use
         Select stateDropdown = new Select(driver.findElement(By.xpath("//select[@id='state']")));
@@ -41,6 +47,11 @@ public class DropdownPractices {
         String actualSelectedOption = stateDropdown.getFirstSelectedOption().getText();//getFirstSelectedOption() brings us the current selection
 
         Assert.assertEquals(actualSelectedOption, expectedSelectedOption);
+    }
+
+    @Test
+    public void dropdown_task6(){
+
     }
 
 }
