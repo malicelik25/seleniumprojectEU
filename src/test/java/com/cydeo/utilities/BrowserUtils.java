ac3a1540ded43737986b4dc1ff33af7c6c03e47b
Muhammed Ali Çelik <muhammedalicelikk@gmail.com>
2022-05-21 14:36:13 +0300

    Browser Utils

--- src/test/java/com/cydeo/utilities/BrowserUtils.java
+++ src/test/java/com/cydeo/utilities/BrowserUtils.java
@@ -24,6 +24,14 @@ public class BrowserUtils {
         }
     }
 
+    /*
+    This method accepts 3 arguments.
+    Arg1: webdriver
+    Arg2: expectedInUrl: for verify if the url contains given String
+          - if condition matches, will break loop.
+          Arg3: expectedTitle to be compared against actualTitle
+     */
+
 
     public static void switchWindowAndVerify(WebDriver driver, String expectedInUrl, String expectedTitle){
 
