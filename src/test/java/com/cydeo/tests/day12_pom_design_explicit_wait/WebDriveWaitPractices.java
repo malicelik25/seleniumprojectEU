98b9db0a07b8868b51e8cd9678d12ccd8211a81c
Muhammed Ali Çelik <muhammedalicelikk@gmail.com>
2022-06-08 20:58:08 +0300

    WebDriveWaitPractices

--- src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/WebDriveWaitPractices.java
+++ src/test/java/com/cydeo/tests/day12_pom_design_explicit_wait/WebDriveWaitPractices.java
@@ -1,5 +1,6 @@
 package com.cydeo.tests.day12_pom_design_explicit_wait;
 
+import com.cydeo.pages.DynamicLoad1Page;
 import com.cydeo.pages.DynamicLoad7Page;
 import com.cydeo.utilities.Driver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -10,7 +11,7 @@ import org.testng.annotations.Test;
 public class WebDriveWaitPractices {
 
     @Test
-    public void dynamic_load_7_test() {
+    public void dynamic_load_7_test(){
         //1. Go to https://practice.cydeo.com/dynamic_loading/7
         Driver.getDriver().get("https://practice.cydeo.com/dynamic_loading/7");
 
@@ -34,17 +35,34 @@ public class WebDriveWaitPractices {
     }
 
     @Test
-    public void dynamic_load_1_test(){
+    public void dynamic_load_1_test() {
         //1. Go to https://practice.cydeo.com/dynamic_loading/1
         Driver.getDriver().get("https://practice.cydeo.com/dynamic_loading/1");
 
+        DynamicLoad1Page dynamicLoad1Page = new DynamicLoad1Page();
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), 10);
+
         //2. Click to start
+        dynamicLoad1Page.startButton.click();
+
         //3. Wait until loading bar disappears
+        wait.until(ExpectedConditions.invisibilityOf(dynamicLoad1Page.loadingBar));
+
         //4. Assert username inputbox is displayed
+        Assert.assertTrue(dynamicLoad1Page.inputUsername.isDisplayed());
+
         //5. Enter username: tomsmith
+        dynamicLoad1Page.inputUsername.sendKeys("tomsmith");
+
         //6. Enter password: incorrectpassword
+        dynamicLoad1Page.inputPassword.sendKeys("incorrectpassword");
+
         //7. Click to Submit button
+        dynamicLoad1Page.submitButton.click();
+
         //8. Assert “Your password is invalid!” text is displayed.
+        Assert.assertTrue(dynamicLoad1Page.errorMessage.isDisplayed());
+
     }
 
 }
